458079144491bc6cab3fa9b8bab3316059b1255f
bean <mailxbs@126.com>
2021-09-01 01:26:25 +0800

    增加ES对日期的支持

--- src/pages/doc/NkDefDocTypeDataSyncs.vue
+++ src/pages/doc/NkDefDocTypeDataSyncs.vue
@@ -19,12 +19,16 @@
             :edit-config="{trigger: 'click', mode: 'row', showIcon: editMode, activeMethod: ()=>{return editMode}}"
             :data="docDef.dataSyncs"
             @edit-actived="editActive">
-            <vxe-table-column   title="服务"             field="targetSvr" width="15%" :edit-render="{
+            <vxe-table-column   title="服务"             field="targetSvr" width="13%" :edit-render="{
                 name: '$select',
-                options: docOptions.docDataSyncs,
+                options: docOptions.docDataSyncAdapters,
                 optionProps: {value: 'key', label: 'name'}
             }"/>
-            <vxe-table-column   title="目标"             field="targetArgs" width="14%" :edit-render="{name: 'input', attrs: {type: 'text'}}"/>
+            <vxe-table-column   title="目标"             field="targetArgs" width="12%" :edit-render="{name: 'input', attrs: {type: 'text'}}"/>
+            <vxe-table-column   title="重建"             field="reExecute" width="8%" :formatter="({cellValue})=>cellValue?'是':''" :edit-render="{
+                name: '$switch',
+                props: {'open-value':1,'close-value':0},
+            }" v-if="docDef.docClassify==='TRANSACTION'||docDef.docClassify==='PROJECT'"/>
             <vxe-table-column   title="条件"             field="conditionSpEL" width="10%" :edit-render="{}">
                 <template v-slot:edit="{row}">
                     <nk-sp-el-editor v-model="row.conditionSpEL"></nk-sp-el-editor>
@@ -35,12 +39,12 @@
                     <nk-sp-el-editor v-model="row.dataSpEL"></nk-sp-el-editor>
                 </template>
             </vxe-table-column>
-            <vxe-table-column   title="主键规则模版"        field="keySpEL" width="18%" :edit-render="{}">
+            <vxe-table-column   title="主键规则模版"        field="keySpEL" width="15%" :edit-render="{}">
                 <template v-slot:edit="{row}">
                     <nk-sp-el-editor v-model="row.keySpEL"></nk-sp-el-editor>
                 </template>
             </vxe-table-column>
-            <vxe-table-column   title="字段映射模版"      field="mappingSpEL" width="18%" :edit-render="{}">
+            <vxe-table-column   title="字段映射模版"      field="mappingSpEL" width="15%" :edit-render="{}">
                 <template v-slot:edit="{row}">
                     <nk-sp-el-template-editor v-model="row.mappingSpEL"></nk-sp-el-template-editor>
                 </template>
@@ -87,6 +91,7 @@ export default {
             const row = {
                 targetSvr:'NkDatabaseAdapter',
                 targetArgs:'<TABLE_NAME>',
+                reExecute: 1,
                 dataSpEL:'',
                 keySpEL:'',
                 mappingSpEL:''
