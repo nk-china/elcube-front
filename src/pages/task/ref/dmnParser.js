7f1051b71cb352c084faaf49688d4873c9e800d8
bean <mailxbs@126.com>
2021-09-29 15:29:26 +0800

    增加决策引擎

--- src/pages/task/ref/dmnParser.js
+++ src/pages/task/ref/dmnParser.js
@@ -1,41 +1,71 @@
 
 
+// function parseDecision(elements, id, inputs, outputs){
+//
+//     const decision = id ? elements.find(e=>e.id===id) : elements.find(e=>e.$type==='dmn:Decision');
+//
+//     if(decision.decisionLogic.$type==='dmn:DecisionTable'){
+//         decision.decisionLogic.input.forEach(e=>{
+//
+//             let findIt = false;
+//             decision.informationRequirement
+//                 .filter(ir=>ir.requiredDecision)
+//                 .find(ir=>{
+//                     const inputId = ir.requiredDecision.href.substr(1);
+//                     const decision_req = elements.find(e=>e.id===inputId);
+//                     const decision_out = decision_req.decisionLogic.output.find(o=>o.name===e.inputExpression.text);
+//                     findIt = decision_out !== undefined;
+//                 })
+//
+//             if(!findIt)
+//                 inputs.push({key:e.inputExpression.text,name:e.label});
+//         })
+//         decision.decisionLogic.output.forEach(i=>{
+//             outputs.push(Object.assign({decisionId:decision.id},i));
+//         })
+//     }else{
+//         decision.informationRequirement.forEach(ir=>{
+//             if(ir.requiredInput){
+//                 const inputId = ir.requiredInput.href.substr(1);
+//                 const input = elements.find(e=>e.id===inputId);
+//                 inputs.push({
+//                     key:  input.id,
+//                     name: input.name
+//                 });
+//             }
+//         })
+//         outputs.push(Object.assign({decisionId:decision.id},decision.variable));
+//     }
+//
+//     decision.informationRequirement.forEach(ir=>{
+//         if(ir.requiredDecision){
+//             const inputId = ir.requiredDecision.href.substr(1);
+//             parseDecision(elements,inputId,inputs,outputs);
+//         }
+//     })
+// }
 function parseDecision(elements, id, inputs, outputs){
 
     const decision = id ? elements.find(e=>e.id===id) : elements.find(e=>e.$type==='dmn:Decision');
 
-    // if(decision.decisionLogic.$type==='dmn:DecisionTable'){
-    //     decision.decisionLogic.input.forEach(e=>{
-    //
-    //         let findIt = false;
-    //         decision.informationRequirement
-    //             .filter(ir=>ir.requiredDecision)
-    //             .find(ir=>{
-    //                 const inputId = ir.requiredDecision.href.substr(1);
-    //                 const decision_req = elements.find(e=>e.id===inputId);
-    //                 const decision_out = decision_req.decisionLogic.output.find(o=>o.name===e.inputExpression.text);
-    //                 findIt = decision_out !== undefined;
-    //             })
-    //
-    //         if(!findIt)
-    //             inputs.push({key:e.inputExpression.text,name:e.label});
-    //     })
-    //     decision.decisionLogic.output.forEach(i=>{
-    //         outputs.push(Object.assign({decisionId:decision.id},i));
-    //     })
-    // }else{
-        decision.informationRequirement.forEach(ir=>{
-            if(ir.requiredInput){
-                const inputId = ir.requiredInput.href.substr(1);
-                const input = elements.find(e=>e.id===inputId);
-                inputs.push({
-                    key:  input.id,
-                    name: input.name
-                });
-            }
+    decision.informationRequirement.forEach(ir=>{
+        if(ir.requiredInput){
+            const inputId = ir.requiredInput.href.substr(1);
+            const input = elements.find(e=>e.id===inputId);
+            inputs.push({
+                key:  input.id,
+                name: input.name
+            });
+        }
+    })
+
+    if(decision.decisionLogic.$type==='dmn:DecisionTable'){
+        decision.decisionLogic.output.forEach(i=>{
+            outputs.push(Object.assign({decisionId:decision.id},i));
         })
+    }else{
         outputs.push(Object.assign({decisionId:decision.id},decision.variable));
-    // }
+    }
 
     decision.informationRequirement.forEach(ir=>{
         if(ir.requiredDecision){
