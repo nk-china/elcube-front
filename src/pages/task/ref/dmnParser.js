9af120e722d4df505a08cb76facebb96f0dd9eab
bean <mailxbs@126.com>
2021-09-29 15:27:27 +0800

    增加决策引擎

--- src/pages/task/ref/dmnParser.js
+++ src/pages/task/ref/dmnParser.js
@@ -4,26 +4,26 @@ function parseDecision(elements, id, inputs, outputs){
 
     const decision = id ? elements.find(e=>e.id===id) : elements.find(e=>e.$type==='dmn:Decision');
 
-    if(decision.decisionLogic.$type==='dmn:DecisionTable'){
-        decision.decisionLogic.input.forEach(e=>{
-
-            let findIt = false;
-            decision.informationRequirement
-                .filter(ir=>ir.requiredDecision)
-                .find(ir=>{
-                    const inputId = ir.requiredDecision.href.substr(1);
-                    const decision_req = elements.find(e=>e.id===inputId);
-                    const decision_out = decision_req.decisionLogic.output.find(o=>o.name===e.inputExpression.text);
-                    findIt = decision_out !== undefined;
-                })
-
-            if(!findIt)
-                inputs.push({key:e.inputExpression.text,name:e.label});
-        })
-        decision.decisionLogic.output.forEach(i=>{
-            outputs.push(Object.assign({decisionId:decision.id},i));
-        })
-    }else{
+    // if(decision.decisionLogic.$type==='dmn:DecisionTable'){
+    //     decision.decisionLogic.input.forEach(e=>{
+    //
+    //         let findIt = false;
+    //         decision.informationRequirement
+    //             .filter(ir=>ir.requiredDecision)
+    //             .find(ir=>{
+    //                 const inputId = ir.requiredDecision.href.substr(1);
+    //                 const decision_req = elements.find(e=>e.id===inputId);
+    //                 const decision_out = decision_req.decisionLogic.output.find(o=>o.name===e.inputExpression.text);
+    //                 findIt = decision_out !== undefined;
+    //             })
+    //
+    //         if(!findIt)
+    //             inputs.push({key:e.inputExpression.text,name:e.label});
+    //     })
+    //     decision.decisionLogic.output.forEach(i=>{
+    //         outputs.push(Object.assign({decisionId:decision.id},i));
+    //     })
+    // }else{
         decision.informationRequirement.forEach(ir=>{
             if(ir.requiredInput){
                 const inputId = ir.requiredInput.href.substr(1);
@@ -35,7 +35,7 @@ function parseDecision(elements, id, inputs, outputs){
             }
         })
         outputs.push(Object.assign({decisionId:decision.id},decision.variable));
-    }
+    // }
 
     decision.informationRequirement.forEach(ir=>{
         if(ir.requiredDecision){
