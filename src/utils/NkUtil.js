3e6f0c3319f8440abb2744ac9e85242622c212b2
bean <mailxbs@126.com>
2021-11-02 00:10:09 +0800

    优化自定义查询，增加对sql的支持

--- src/utils/NkUtil.js
+++ src/utils/NkUtil.js
@@ -29,70 +29,71 @@ export default {
         }
         return false;
     },
-    toEsParams(params,preCondition,$debug){
-
-        let must = [],
-            np = {};
-        let value;
-        for(let field in params){
-            switch (field) {
-                case "_source":
-                    //如果$debug 为true，不设置索引返回字段，服务器默认返回全部字段
-                    if(!$debug){
-                        np[field]=params[field];
-                    }
-                    break;
-                case "$aggs":
-                case "from":
-                case "rows":
-                case "keyword":
-                case "order":
-                case "orderField":
-                case "_keywordField":
-                    np[field]=params[field];
-                    break;
-                case "_highlight":{
-                    let highlightFields = [];
-                    let highlight = params[field];
-                    for(let k in highlight){
-                        if(highlight[k]){
-                            highlightFields.push(k);
-                        }
-                    }
-                    np[field]=highlightFields.join(',');
-                    break;
-                }
-                case "conditions":break;
-                default:
-                    value = params[field];
-                    if(value instanceof Array){
-                        if(value.length){
-                            let terms = {};
-                            terms[field]=value;
-                            must.push({
-                                terms
-                            });
-                        }
-                    }else if(value instanceof Object){
-                        let range = {};
-                        range[field]=value;
-                        must.push({
-                            range
-                        });
-                    }else if(value){
-                        let match = {};
-                        match[field]=`${value}`;
-                        must.push({
-                            match
-                        });
-                    }
-                    break;
-            }
-        }
-        np.condition = must && must.length ? JSON.stringify({"bool": {must}}) : null;
-        if(preCondition)
-            np.preCondition = JSON.stringify(preCondition);
-
-        return np;
-    }
+    // toEsParams(params,preCondition,$debug){
+    //
+    //     let must = [],
+    //         condition = {"bool": {must}},
+    //         np = {};
+    //     let value;
+    //     for(let field in params){
+    //         switch (field) {
+    //             case "_source":
+    //                 //如果$debug 为true，不设置索引返回字段，服务器默认返回全部字段
+    //                 if(!$debug){
+    //                     np[field]=params[field];
+    //                 }
+    //                 break;
+    //             case "$aggs":
+    //             case "from":
+    //             case "rows":
+    //             case "keyword":
+    //             case "order":
+    //             case "orderField":
+    //             case "_keywordField":
+    //                 np[field]=params[field];
+    //                 break;
+    //             case "_highlight":{
+    //                 let highlightFields = [];
+    //                 let highlight = params[field];
+    //                 for(let k in highlight){
+    //                     if(highlight[k]){
+    //                         highlightFields.push(k);
+    //                     }
+    //                 }
+    //                 np[field]=highlightFields.join(',');
+    //                 break;
+    //             }
+    //             case "conditions":break;
+    //             default:
+    //                 value = params[field];
+    //                 if(value instanceof Array){
+    //                     if(value.length){
+    //                         let terms = {};
+    //                         terms[field]=value;
+    //                         must.push({
+    //                             terms
+    //                         });
+    //                     }
+    //                 }else if(value instanceof Object){
+    //                     let range = {};
+    //                     range[field]=value;
+    //                     must.push({
+    //                         range
+    //                     });
+    //                 }else if(value){
+    //                     let match = {};
+    //                     match[field]=`${value}`;
+    //                     must.push({
+    //                         match
+    //                     });
+    //                 }
+    //                 break;
+    //         }
+    //     }
+    //     np.condition = JSON.stringify(condition);
+    //     if(preCondition)
+    //         np.preCondition = JSON.stringify(preCondition);
+    //
+    //     return np;
+    // }
 }
