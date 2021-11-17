b6f57e6b99e58d100c3e2e60d82df8ce1105b3ed
bean <mailxbs@126.com>
2021-11-17 19:52:19 +0800

    修复组件404

--- src/pages/doc/NkDefScriptLabel.vue
+++ src/pages/doc/NkDefScriptLabel.vue
@@ -2,19 +2,19 @@
     <span v-if="beanDescribe">
         <template v-if="beanDescribe.groovy">
             <template v-if="beanDescribe.debug">
-                <router-link :to="`/apps/def/script/detail/${beanDescribe.className}/${beanDescribe.version}`" style="color: #fa8c16">{{value}}</router-link>
+                <router-link :to="`/apps/def/component/detail/${beanDescribe.className}/${beanDescribe.version}`" style="color: #fa8c16">{{value}}</router-link>
                 <a-tag color="orange" style="margin-left: 5px;">Debug</a-tag>
             </template>
             <template v-else-if="beanDescribe.state==='Active'">
-                <router-link :to="`/apps/def/script/detail/${beanDescribe.className}/${beanDescribe.version}`" style="color: #52c41a">{{value}}</router-link>
+                <router-link :to="`/apps/def/component/detail/${beanDescribe.className}/${beanDescribe.version}`" style="color: #52c41a">{{value}}</router-link>
                 <a-tag color="green"  style="margin-left: 5px;">Active</a-tag>
             </template>
             <template v-else-if="beanDescribe.state==='InActive'">
-                <router-link :to="`/apps/def/script/detail/${beanDescribe.className}/${beanDescribe.version}`" style="color: #1890ff">{{value}}</router-link>
+                <router-link :to="`/apps/def/component/detail/${beanDescribe.className}/${beanDescribe.version}`" style="color: #1890ff">{{value}}</router-link>
                 <a-tag color="blue"   style="margin-left: 5px;">InActive</a-tag>
             </template>
             <template v-else-if="beanDescribe.state==='Native'">
-                <router-link :to="`/apps/def/script/detail/${beanDescribe.className}/${beanDescribe.version}`" style="color: #1890ff">{{value}}</router-link>
+                <router-link :to="`/apps/def/component/detail/${beanDescribe.className}/${beanDescribe.version}`" style="color: #1890ff">{{value}}</router-link>
                 <a-tag color="blue"   style="margin-left: 5px;">Native</a-tag>
             </template>
         </template>
