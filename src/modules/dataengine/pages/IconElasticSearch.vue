280bf039f563023560c8b79a1c83956b8060823e
bean <mailxbs@126.com>
2021-12-02 13:36:01 +0800

    This files is part of EAsis. EAsis is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. EAsis is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero General Public License along with EAsis.  If not, see <https://www.gnu.org/licenses/>.

--- src/modules/dataengine/pages/IconElasticSearch.vue
+++ src/modules/dataengine/pages/IconElasticSearch.vue
@@ -1,3 +1,16 @@
+<!--
+	This file is part of EAsis.
+	EAsis is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	EAsis is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
+	You should have received a copy of the GNU Affero General Public License
+	along with EAsis.  If not, see <https://www.gnu.org/licenses/>.
+-->
 <template>
     <svg width="24px" height="24px" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
         <title>logo-elastic-search-24-color</title>
--- src/modules/docengine/page/NkDevOpsDataSync.vue
+++ src/modules/docengine/page/NkDevOpsDataSync.vue
@@ -1,3 +1,16 @@
+<!--
+	This file is part of EAsis.
+	EAsis is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	EAsis is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
+	You should have received a copy of the GNU Affero General Public License
+	along with EAsis.  If not, see <https://www.gnu.org/licenses/>.
+-->
 <template>
     <nk-page-layout title="数据同步管理" sub-title="管理ElasticSearch索引、数据映射等">
 
