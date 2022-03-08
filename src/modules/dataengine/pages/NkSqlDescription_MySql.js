16f2eee37883a340f32b6aa71bd19c91155b62c8
bean <mailxbs@126.com>
2022-03-08 13:38:25 +0800

    增加数据发现对MySQL的支持

--- src/modules/dataengine/pages/NkSqlDescription_MySql.js
+++ src/modules/dataengine/pages/NkSqlDescription_MySql.js
@@ -13,21 +13,6 @@
  */
 
 
-const text = {
-    alias:{
-        label: '别名',
-        type: 'a-input',
-        required: false,
-        class:{width:'220px'}
-    },
-    $format(e){
-        e.$format = {
-            alias : e.alias || e.name,
-            select:`${e.name} AS "${e.alias || e.name}"`
-        };
-    }
-}
-
 const keyword = {
     alias:{
         label: '别名',
@@ -56,7 +41,6 @@ const keyword = {
     having:{
         label: '聚合筛选',
         type: 'a-select',
-        required: false,
         visible(e){return !e.group&&e.aggregation==='COUNT';},
         options(e){
             if(e.aggregation==='COUNT')
@@ -101,22 +85,85 @@ const date = {
         required: false,
         class:{width:'220px'}
     },
+    aggregation:{
+        required:false,
+        label: '聚合',
+        type: 'a-select',
+        options:[
+            {value:undefined,label:'NONE'},
+            {value:"COUNT",  label:'计数'},
+            {value:"MIN",    label:'最小的'},
+            {value:"MAX",    label:'最大的'}
+        ],
+        class:{width:'160px'}
+    },
+    having:{
+        required:false,
+        label: '聚合筛选',
+        type: 'a-select',
+        visible(e){return e.aggregation;},
+        options:[
+            {value:undefined,   label:'NONE'  },
+            {value:"=",         label:"=",    },
+            {value:">=",        label:">=",   },
+            {value:">",         label:">",    },
+            {value:"<=",        label:"<=",   },
+            {value:"<",         label:"<",    },
+            {value:"<>",        label:"<>",   },
+            {value:"BETWEEN",   label:"介于",  }
+        ],
+        class:{width:'120px'}
+    },
+    havingValue:{
+        label: '值',
+        type(e){return e.aggregation==='COUNT'?'nk-number-range':'nk-date-range';},
+        mode(e){return e.having==='BETWEEN'?'range':'single';},
+        visible(e){return !e.group&&e.aggregation&&e.having;},
+        class(e){return e.symbol === 'BETWEEN' ? {width:'320px'} : {width:'220px'}},
+    },
+    $format(e){
+        const select = e.group ? (
+            `toStartOfInterval(${e.name}, INTERVAL ${e.interval} ${e.intervalType}, 'Asia/Shanghai')`
+        ):(
+            e.aggregation ? (e.aggregation+'('+e.name+')') : e.name
+        );
+        const quote = e.aggregation!=='COUNT'?'\'':'';
+        const alias = e.alias || (e.name + (e.group ? '_HISTOGRAM' : '') +( e.aggregation ? ('_'+e.aggregation):'') );
+        const value = quote + ((e.having === "BETWEEN" && e.havingValue) ? e.havingValue.join(quote+' AND '+quote) : e.havingValue) + quote;
+
+        e.$format = {
+            alias,
+            select:`${select} AS "${alias}"`,
+            group: e.group  && `"${alias}"`,
+            having:e.having && `"${alias}" ${e.having} ${value}`
+        };
+    }
+};
+
+
+const datetime = {
+    alias:{
+        label: '别名',
+        type: 'a-input',
+        required: false,
+        class:{width:'220px'}
+    },
     group:{
         required:false,
         label: '分组',
         type: 'a-select',
-        options:[{label:'不分组',value:undefined},{label:'时间线',value:"HISTOGRAM"}],
+        options:[{label:'不分组',value:undefined},{label:'时间线',value:"toStartOfInterval"}],
         class:{width:'160px'}
     },
     interval:{
         label: '周期',
         type: 'a-input-number',
-        visible(e){return e.group === 'HISTOGRAM';}
+        visible(e){return e.group === 'toStartOfInterval';}
     },
     intervalType:{
         label: '类型',
         type: 'a-select',
-        visible(e){return e.group === 'HISTOGRAM';},
+        visible(e){return e.group === 'toStartOfInterval';},
         options:[{label:'年',value:'YEAR'}, {label:'月',value:'MONTH'}, {label:'日',value:'DAY'}],
         class:{width:'140px'}
     },
@@ -134,7 +181,6 @@ const date = {
         class:{width:'160px'}
     },
     having:{
-        required: false,
         label: '聚合筛选',
         type: 'a-select',
         visible(e){return !e.group&&e.aggregation;},
@@ -159,7 +205,7 @@ const date = {
     },
     $format(e){
         const select = e.group ? (
-            `HISTOGRAM(${e.name}, INTERVAL ${e.interval} ${e.intervalType})`
+            `toStartOfInterval(${e.name}, INTERVAL ${e.interval} ${e.intervalType}, 'Asia/Shanghai')`
         ):(
             e.aggregation ? (e.aggregation+'('+e.name+')') : e.name
         );
@@ -184,9 +230,10 @@ const number = {
         class:{width:'220px'}
     },
     group:{
+        visible: false,
+        required: false,
         label: '分组',
         type: 'a-switch',
-        required: false,
     },
     interval:{
         label: '间隔',
@@ -194,7 +241,7 @@ const number = {
         visible(e){return e.group;}
     },
     aggregation:{
-        required: false,
+        required:false,
         label: '聚合',
         type: 'a-select',
         visible(e){return !e.group;},
@@ -209,7 +256,7 @@ const number = {
         class:{width:'160px'}
     },
     having:{
-        required: false,
+        required:false,
         label: '聚合筛选',
         type: 'a-select',
         visible(e){return !e.group&&e.aggregation;},
@@ -387,27 +434,21 @@ const numberFilter = {
 }
 
 export default {
-    formatName(index){
-        return `"${index}"`;
-    },
     fields:{
-        text,
-        keyword,
-        date,
         short:number,
-        integer:number,
-        long:number,
-        float:number,
-        double:number
+        int:number,
+        bigint:number,
+        varchar:keyword,
+        date:date,
+        datetime:datetime
+
     },
     filters:{
-        text:keywordFilter,
-        keyword:keywordFilter,
-        date:dateFilter,
         short:numberFilter,
-        integer:numberFilter,
-        long:numberFilter,
-        float:numberFilter,
-        double:numberFilter,
+        int:numberFilter,
+        bigint:numberFilter,
+        varchar:keywordFilter,
+        date:dateFilter,
+        datetime:dateFilter
     }
 }
